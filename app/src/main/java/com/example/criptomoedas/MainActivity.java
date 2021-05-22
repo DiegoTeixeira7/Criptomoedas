85e32c21d507b76eff16a55d5a1f09597077a366
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2021-05-22 16:33:38 -0300

    att

--- app/src/main/java/com/example/criptomoedas/MainActivity.java
+++ app/src/main/java/com/example/criptomoedas/MainActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static java.lang.Double.parseDouble;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView response;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +19,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void calcular(View view) {
+        String qtdBtc = ((EditText)findViewById(R.id.qtdBtc)).getText().toString();
+        String valueBtc = ((EditText)findViewById(R.id.valueBtc)).getText().toString();
+
+        String qtdEth = ((EditText)findViewById(R.id.qtdEth)).getText().toString();
+        String valueEth = ((EditText)findViewById(R.id.valueEth)).getText().toString();
+
+        String qtdXrp = ((EditText)findViewById(R.id.qtdXrp)).getText().toString();
+        String valueXrp = ((EditText)findViewById(R.id.valueXrp)).getText().toString();
+
+        double resp = (parseDouble(qtdBtc) * parseDouble(valueBtc)) +
+                (parseDouble(qtdEth) * parseDouble(valueEth)) +
+                (parseDouble(qtdXrp) * parseDouble(valueXrp));
+
+        response = findViewById(R.id.textResp);
+        response.setText("R$" + resp);
     }
 }
